7eed02157b2d740914c80fe27119639801d2da37
leo <leo00932302@gmail.com>
2026-03-31 18:13:51 +0800

    做完需要的函數，有簡單測試過

--- function.c
+++ function.c
@@ -1,7 +1,5 @@
 #include"function.h"
 
-
-
 char *command_line_str[] = {
   "cd",
   "help",
@@ -22,6 +20,26 @@ int lsh_cd(char **args){
     }
   }
   return 1;
+}
+
+int lsh_help(char **args)
+{
+  int i;
+  printf("the program is developed by shengli in 20260331 from Stephen Brennan's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < lsh_num_commandline(); i++) {
+    printf("  %s\n", command_line_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args)
+{
+  return 0;
 }
 
 int (*command_line_func[])(char **) = {
