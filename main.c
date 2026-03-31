02b8cc6e1d82b9fa908cc7bf7f5659b80ad4e51b
leo <leo00932302@gmail.com>
2026-03-31 18:15:24 +0800

    測試loop ok 測試三階段執行 ok 整體程式運作正常可以編譯 ok

--- main.c
+++ main.c
@@ -4,15 +4,79 @@
 #include<errno.h>
 #include"function.h"
 
-char* lsh_read_line();
-char** lsh_split_line();
-int lsh_execute();
+
+
+char* lsh_read_line(){
+  char *line = NULL;
+  size_t bufsize = 0; // have getline allocate a buffer for us
+  if (getline(&line, &bufsize, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);  // We received an EOF
+    } else  {
+      perror("lsh: getline\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+};
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char** lsh_split_line(char *line){
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+};
+
+
+int lsh_execute(char **args){
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_commandline(); i++) {
+    if (strcmp(args[0], command_line_str[i]) == 0) {
+      return (*command_line_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
+};
 
 void lsh_loop(void){
   char *line;
   char **args;
   int status;
-  while(0){
+  while(1){
     printf(">");
     line = lsh_read_line();
     args = lsh_split_line(line);
@@ -23,6 +87,8 @@ void lsh_loop(void){
 int lsh_launch(char **args){
   pid_t pid;
   int status;
+
+  pid = fork();
   if(pid == 0){
 
     if(execvp(args[0],args) == -1){
@@ -45,13 +111,10 @@ int lsh_launch(char **args){
     }
     
     
-  
+  return 1;
 }
 
-int lsh_execute(char **args){
-  int i;
-
-  if(args[0] == NULL){
-    return 1; //empty command
-  }
+int main() {
+  lsh_loop();
+  return 0;
 }
