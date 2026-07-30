3b0a94741c940daeaf557b7f7be28e9c1e51586a
leo <leo00932302@gmail.com>
2026-07-31 02:27:31 +0800

    除了split都測好了

--- main.c
+++ main.c
@@ -1,18 +1,96 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#define LAUNCH 0
 
-char *command_line_str[] = {"help","exit"};
+/*
+@function are declared and definited here
+*/
+int help(char** args){
+    printf("the shell was developed by sheng li,and it was very simple\n");
+};
+int Exit(char** args){
+  return 0;
+};
 
+/*
+@set up the command names and function pointers
+*/
+char *command_line_str[] = {"help","Exit"};
+int (*command_line_func[])(char **) = {&help};
 
+/*
+@it can fork the process
+@it still test,Launch is set 0
+*/
+#ifdef LAUNCH 
+int shell_launch(char** args){
+}
+#endif
 
+/*
+@count the number of functions
+*/
+int the_number_of_commandline(){
+    return sizeof(command_line_str)/sizeof(char*);
+};
+//the return value is status
+int shell_execute(char** args){
+    //input is empty
+    if(args[0] == NULL) return 1;
+    //copared the input args with commandline_string,if it mached,the function will be execued
+    for(int i=0;i<the_number_of_commandline();i++){
+        if(strcmp(args[0],command_line_str[i]) == 0){
+           return (*command_line_func[i])(args);  
+           //the function will be executed and the return value type will be an integer
+        }
+    }
+    return 1; //we don't find out the command
+};
 
+/*
+@the steps will get the input and split it into command
+@there are three steps:read,split,loop
+*/
+char *read_line(void){
+  char *line = NULL;
+  ssize_t bufsize = 0;  //getline return type is ssize_t, that is long
+  if(getline(&line,&bufsize,stdin) == -1){
+    if (feof(stdin))
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else{
+        perror("getline\n");
+        exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+};
+#define BUFFSIZE 100
+char **split_line(char *line){
+  int bufsize = BUFFSIZE;
+  char **tokens = (char**)malloc(bufsize*(sizeof(char*)));
+  char token, **tokens_backup;
+  
+};
+void shell_loop(void){
+  char *line;
+  char **args;
+  int status;
 
-
-
-
-
+  do
+  {
+    printf(">");
+    line = read_line();
+    args = split_line(line);
+    status = shell_execute(args);
+  } while (status);
+  
+};
 
 int main(void){
-
+shell_loop();
     return 0;
 }
