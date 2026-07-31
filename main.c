31512eab9ac0aef11ba493e654bc50ee745b045f
leo <leo00932302@gmail.com>
2026-07-31 23:13:54 +0800

    做完了，可以運作，但還有很多警告要修正，也還沒有新增fork功能

--- main.c
+++ main.c
@@ -68,12 +68,38 @@ char *read_line(void){
   }
   return line;
 };
-#define BUFFSIZE 100
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 char **split_line(char *line){
-  int bufsize = BUFFSIZE;
-  char **tokens = (char**)malloc(bufsize*(sizeof(char*)));
-  char token, **tokens_backup;
-  
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "shell: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 };
 void shell_loop(void){
   char *line;
